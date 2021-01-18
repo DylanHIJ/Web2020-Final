a796f074549b5ccd367e117bb4d2c5b637aa69c1
Wu-Jun Pei <jimpei8989@gmail.com>
2021-01-18 21:45:23 +0800

    Add root component

---
+++ ntu-cooler/src/components/teachers/AddProblems.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+
+const AddProblem = (props) => {
+  const { assignmentID } = props;
+
+  const classes = useStyles();
+
+  const assignment = getAssignment(assignmentID);
+  const students = getStudentList(assignmentID).sort((a, b) =>
+    a.studentID <= b.studentID ? -1 : 1
+  );
+
+  return (
+    <Container maxWidth="lg">
+      {/* Assignment Name */}
+      Root for addProblems.
+    </Container>
+  );
+};
+
+export default AddProblem;
